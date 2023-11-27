82282d080492194a8182727c6626db65d789b161
sinnom <106713618+sinnom@users.noreply.github.com>
2023-11-27 11:38:12 -0600

    feat: add `GameStates` to track menu or ingame status

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 use bevy::prelude::*;
 
+mod menu;
+
+#[derive(States, Debug, Default, Hash, PartialEq, Eq, Clone)]
+enum GameStates {
+    #[default]
+    MainMenu,
+    InGame,
+    /// AKA Pause menu or escape menu
+    InGameMenu,
+}
+
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_state::<GameStates>()
+        .run();
 }
